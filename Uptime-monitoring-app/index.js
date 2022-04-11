dfcd2986ce295c67f47ee7b4d1813c9ba95d1b37
ibnyahyah <yahyahridwan665@gmail.com>
2022-04-11 08:42:21 -0700

    adding new twillio keys

--- Uptime-monitoring-app/index.js
+++ Uptime-monitoring-app/index.js
@@ -1,29 +1,29 @@
-/* 
-*Primary File for the API
-*
-*/
+/*
+ *Primary File for the API
+ *
+ */
 
 // Dependencies
 var server = require("./lib/server");
 var workers = require("./lib/workers");
-var cli = require('./lib/cli');
+var cli = require("./lib/cli");
 
 // Declare the app
 var app = {};
 
 // Init Function
-app.init = function(){
-    // Start the server
-    server.init();
+app.init = function () {
+  // Start the server
+  server.init();
 
-    // Start the worker
-    workers.init();
+  // Start the worker
+  workers.init();
 
-    // Start the CLi,but make sure it satarts last
-    setTimeout(function(){
-        cli.init();
-    },50)
-}
+  // Start the CLi,but make sure it satarts last
+  setTimeout(function () {
+    cli.init();
+  }, 50);
+};
 
 // Execute the function
 app.init();
--- Uptime-monitoring-app/lib/config.js
+++ Uptime-monitoring-app/lib/config.js
@@ -7,48 +7,54 @@ var environments = {};
 
 // Staging {default} environment
 environments.staging = {
-    'httpPort':3000,
-    'httpsPort':3001,
-    'envName':'staging',
-    'hashingSecret':'thisIsASecret',
-    'maxChecks':5,
-    'twilio' : {
-    'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
-    'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
-    'fromPhone' : '+15005550006'
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: "staging",
+  hashingSecret: "thisIsASecret",
+  maxChecks: 5,
+  twilio: {
+    accountSid: "AC560d7222ee03ae46f97f6acf94f5df7f",
+    authToken: "faf49cae96c5a9927376770c9001360a",
+    fromPhone: "+19378822597",
   },
-  'templateGlobals':{
-    'appName':'UptimeChecker',
-    'companyName':'NotARealCompany',
-    'yearCreated':'2018',
-    'baseUrl':'http://localhost:3000/'
-  }
+  templateGlobals: {
+    appName: "UptimeChecker",
+    companyName: "NotARealCompany",
+    yearCreated: "2022",
+    baseUrl: "http://localhost:3000/",
+  },
 };
 
 // production envirionment
 environments.production = {
-    'httpPort':5000,
-    'httpsPort':5001,
-    'envName':'production',
-    'hashingSecret':'thisIsASecret',
-    'maxChecks':5,
-    'twilio' : {
-        'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
-        'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
-        'fromPhone' : '+15005550006'
-      },
-    'templateGlobals':{
-      'appName':'UptimeChecker',
-      'companyName':'NotARealCompany',
-      'yearCreated':'2018',
-      'baseUrl':'http://localhost:3001/'
-    }
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: "production",
+  hashingSecret: "thisIsASecret",
+  maxChecks: 5,
+  twilio: {
+    accountSid: "AC560d7222ee03ae46f97f6acf94f5df7f",
+    authToken: "faf49cae96c5a9927376770c9001360a",
+    fromPhone: "+19378822597",
+  },
+  templateGlobals: {
+    appName: "UptimeChecker",
+    companyName: "NotARealCompany",
+    yearCreated: "2018",
+    baseUrl: "http://localhost:3001/",
+  },
 };
 
-// Determine which enviromennt was passed as acommand argument 
-var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : ' ';
+// Determine which enviromennt was passed as acommand argument
+var currentEnvironment =
+  typeof process.env.NODE_ENV == "string"
+    ? process.env.NODE_ENV.toLowerCase()
+    : " ";
 
 // check that the current environment is one of the environment above, if not use default to staging
-var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+var environmentToExport =
+  typeof environments[currentEnvironment] == "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
 
 module.exports = environmentToExport;
